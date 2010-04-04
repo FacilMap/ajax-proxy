c740915ab3be4b7bf27794ccf8e1eb52db9c5f75
Candid Dauth <cdauth@cdauth.de>
2010-04-04 21:40:16 +0200

    Inserted license notice.

--- src/main/webapp/ajax-proxy.js
+++ src/main/webapp/ajax-proxy.js
@@ -1,3 +1,20 @@
+/*
+ * ajax-proxy is a web proxy that tunnels XMLHttpRequests through JavaScript function callbacks.
+ * 
+ * Copyright ⓒ 2010 Candid Dauth
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * An implementation of the XMLHttpRequest interface (http://www.w3.org/TR/XMLHttpRequest/) that
  * works around the Same Origin Policy by using JavaScript function calls that the ajax-proxy
